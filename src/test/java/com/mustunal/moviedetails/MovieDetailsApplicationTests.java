0445b1bd217b5f765153123089d1d1bda30b34aa
mustunal <murat.ustunal@gmail.com>
2022-04-03 22:31:11 +0300

    Initial Commit

---
+++ src/test/java/com/mustunal/moviedetails/MovieDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustunal.moviedetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
